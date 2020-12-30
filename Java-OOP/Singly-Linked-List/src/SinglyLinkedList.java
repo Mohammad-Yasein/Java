ad2334f91a08bff598786bf701eb16f6691aaeac
Mohammad-Yasein <myn.shanabli@gmail.com>
2020-12-31 00:19:24 +0200

    singly linked list assignment

---
+++ Java-OOP/Singly-Linked-List/src/SinglyLinkedList.java
@@ -0,0 +1,41 @@
+public class SinglyLinkedList {
+    public Node head;
+
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node runner = head;
+            while (runner.next != null) {
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+    }
+
+    public void remove() {
+        if (head == null) {
+        } else if (head.next == null) {
+            head = null;
+        } else {
+            Node runner = head;
+            while (runner.next.next != null) {
+                runner = runner.next;
+            }
+            runner.next = null;
+        }
+    }
+
+    public void printValues() {
+        Node runner = head;
+        while (runner != null) {
+            System.out.println(runner.value);
+            runner = runner.next;
+        }
+    }
+}
